6afb8b013c2ede1b0b41822a91a5824c2df43263
Jelil <jeliltuvakov18@gmail.com>
2024-10-07 06:00:29 +0500

    intl version upgraded

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Asus\Desktop\Projects\turkmen_localization_support"
+export "FLUTTER_ROOT=/Users/jelil/src/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jelil/turkmen_localization_support"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.7"
 export "FLUTTER_BUILD_NUMBER=0.0.7"
